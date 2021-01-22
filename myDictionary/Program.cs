511175efa3f8033c125b9a4250cf84ff46b0542f
Serkan Okur <serkan.okur@gmail.com>
2021-01-22 16:40:41 +0100

    Add project files.

---
+++ myDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace myDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> cities = new MyDictionary<int, string>();
+
+            cities.Add(1, "Adana");
+            cities.Add(6, "Ankara");
+
+            
+
+            cities.List();
+
+            Console.WriteLine("================");
+            Console.WriteLine("Is there a city with number 1? : " + cities.ContainsKey(1));
+            Console.WriteLine("Number 1 city is " + cities.GetValue(1));
+            Console.WriteLine("Is there a city named Denizli in cities? : " + cities.ContainsValue("Denizli"));
+            Console.WriteLine("Is there a city named Ankara in cities? : " + cities.ContainsValue("Ankara"));
+            Console.WriteLine("Ankara's number:"+ cities.GetKey("Ankara"));
+            
+        }
+    }
+
+
+}
---
+++ myDictionary/myDictionary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        List<TKey> _keys;
+        List<TValue> _values;
+
+        public MyDictionary()
+        {
+           _keys = new List<TKey>();
+           _values = new List<TValue>();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (_keys.Contains(key))
+            {
+                throw new ArgumentException();
+            }
+            _keys.Add(key);
+            _values.Add(value);
+        }
+
+        public TValue GetValue(TKey key)
+        {
+            int index = _keys.IndexOf(key);
+            return _values[index];
+        }
+
+        public TKey GetKey(TValue val)
+        {
+            int index = _values.IndexOf(val);
+            return _keys[index];
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return _keys.Contains(key);
+        }
+
+        public bool ContainsValue(TValue value)
+        {
+            return _values.Contains(value);
+        }
+
+        public void List()
+        {
+            Console.WriteLine("==== Here is the list: ====");
+            for(var i=0; i<_keys.Count; i++)
+            {
+                Console.WriteLine( _keys[i]+ ":"+ _values[i]);
+            }
+        }
+
+    }
+}
